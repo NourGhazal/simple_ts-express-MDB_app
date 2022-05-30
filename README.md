6ac7f0fe19258714c73370fd6abe7eaea0f5cb1a
unknown <noureldeang@gmail.com>
2022-05-30 17:00:47 +0200

    added  readme, comments and final modifications

---
+++ README.md
@@ -0,0 +1,7 @@
+## How to run:
+- Make sure node.js is installed on your machine
+- Run npm i command in the  command line
+- Install mongoDB on your local machine 
+- run MongoDB instance
+- run "npm run dev" command
+
--- routes/people.ts
+++ routes/people.ts
@@ -6,32 +6,42 @@ const router = express.Router();
 
   router.get("/",async (req:any,res:any)=>{
     try{
+        //retrieving all people data from the database
         const allPeople = await People.find();
-        console.log("HERE")
         return res.status(200).json(allPeople);
     }
-    catch{
-        return res.status(500).json({msg:"Something went wrong :("});
+    catch (error){
+        return res.status(500).json({ message: (error as Error).message });
     }
   });
   router.get("/:id",async (req:Express.Request,res:Express.Response)=>{
     try{
-        console.log("HERE")
         const id = req.params.id;
+        //finding the person by id
         const person = await People.findOne({_id:id});
-        return res.status(200).json(person);
+        if(person){
+            return res.status(200).json(person);
+        }
+        else{
+            return res.status(404).json({msg:"Not found"});
+        }
+        
     }
-    catch{
-        return res.status(500).json({msg:"Something went wrong :("});
+    catch (error){
+        return res.status(500).json({ message: (error as Error).message });
     }
   });
 router.post("/",async (req:Express.Request,res:Express.Response)=>{
     try{
-        console.log("HERE")
+        //getting the data from the request body
         const {firstName, lastName} = req.body;
+        //creating a new person
         const person = new People({firstName, lastName});
-        console.log(person);
+//we could use  new People(req.body); instead of the above but this is better check our attributes
+        // console.log(person);
+        //saving the person in our data base 
         const saved = await person.save();
+        //returning the saved person's id 
         return res.status(201).json({msg:"saved successfully", id:saved._id});
     }
     catch (error){
